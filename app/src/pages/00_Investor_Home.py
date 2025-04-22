5cb6cb72bb424ed099c0416ea326013a3d2a9eb7
Duke Diamond <diamond.d@northeastern.edu>
2025-04-22 15:52:10 -0700

    frontend initialized, investor home & wallet implemented

---
+++ app/src/pages/00_Investor_Home.py
@@ -0,0 +1,45 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome Investor, {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
+
+if st.button('View Wallet',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/01_Wallet.py')
+
+if st.button('View Portfolio',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Portfolio.py')
+
+if st.button('View Projects in the Market',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/03_Projects.py')
+
+if st.button('View Transactions',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/04_Transactions.py')
+
+if st.button('View Educational Crypto Information',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/05_Education.py')
+
+if st.button('View Withdrawals',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/06_Withdrawals.py')
